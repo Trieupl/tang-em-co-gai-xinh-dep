532465ac439353b412f2a8dbf0f5bb01c1ed3c1e
Trieupl <116199028+Trieupl@users.noreply.github.com>
2022-10-20 00:43:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    introTitle: 'Hê sờ nhô cô gái xinh đẹp nhất mà anh từng biết!',
+    introDesc: `Ngày hôm nay ngày mà anh có thể dám nói những lời anh muốn nói với em từ lâu.
+    Không chỉ 20-10 mà tất cả những ngày khác em đều là cô gái xinh đẹp nhất mà anh từng gặp
+    Bởi vì ngay từ lần đầu gặp em anh đã đứng hình mất vài chục giây
+    Ấn tiếp tục đi và anh sẽ có điều bất ngờ cho em`,
+    btnIntro: 'Tiếp tục !',
+    title: 'Hôm nay là 1 ngày đặc biệt của cô gái xinh đẹp nhất mà anh biết 😙',
+    desc: 'Em có tin vào tình yêu sét đánh không. Và khiến em cũng đang thích thầm anh ? Không trả lời là đồng ý đó nha >.< ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnNo: 'Không, Anh nằm mơ à :3',
+    question:'Món quà em muốn vào 20-10 này là gì vậy cô gái <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Món quà duy nhất mà em nghĩ đến chắc là anh <33333333',
+    mess: 'Anh biết mà 🥰. Anh đùa đó nhưng anh vẫn mong nó sẽ thành sự thật 😘😘',
+    messDesc: 'Em có muốn 1 cái hẹn với anh vào ngày đặc biệt này không.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/nguyenkhtrieu' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
